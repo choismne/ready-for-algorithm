1ebc279a304c65fe7b2ed8c15840e86d7ffcbf37
최성민 <sungmin2517@naver.com>
2024-01-16 15:38:20 +0900

    modify

--- greedy/q3.py
+++ greedy/q3.py
@@ -1,24 +1,42 @@
 #문자열 뒤집기
-s = input()
-bin = list(map(int, s))
+# s = input()
+# bin = list(map(int, s))
 
-result_zero=0
-for i in range(len(bin)-1):
-    if len(set(bin))==1:
-        break
-    if bin[i] == 0:
-        bin[i]=1
-        if bin[i+1] == 1:
-            result_zero+=1
+# result_zero=0
+# for i in range(len(bin)-1):
+#     if len(set(bin))==1:
+#         break
+#     if bin[i] == 0:
+#         bin[i]=1
+#         if bin[i+1] == 1:
+#             result_zero+=1
 
-result_one=0
-for i in range(len(bin)-1):
-    if len(set(bin))==1:
-        break
-    if bin[i] == 1:
-        bin[i]=0
-        if bin[i+1] == 0:
-            result_one+=1
+# result_one=0
+# for i in range(len(bin)-1):
+#     if len(set(bin))==1:
+#         break
+#     if bin[i] == 1:
+#         bin[i]=0
+#         if bin[i+1] == 0:
+#             result_one+=1
 
     
-print(min(result_zero, result_one))
+# print(min(result_zero, result_one))
+
+s = input()
+count_zero = 0
+count_one = 0
+
+if s[0] == '1':
+    count_zero+=1
+else:
+    count_one+=1
+
+for i in range(len(s)-1):
+    if s[i] != s[i+1]:
+        if s[i+1] == '1':
+            count_zero+=1
+        else:
+            count_one+=1
+
+print(min(count_zero, count_one))
