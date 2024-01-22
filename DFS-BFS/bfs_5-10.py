1cbafeb324f3619429975a1e3e069a95a519a811
최성민 <sungmin2517@naver.com>
2024-01-22 14:16:28 +0900

    bfs

--- DFS-BFS/bfs_5-10.py
+++ DFS-BFS/bfs_5-10.py
@@ -1,22 +1,41 @@
-# #예시에서는 dfs로 풀지만 bfs로도 풀어보자. 
-# from collections import deque
+from collections import deque
 
-# n, m = map(int, input().split())
+n, m = map(int, input().split())
 
-# graph = []
-# for _ in range(n):
-#     graph.append(list(map(int, input())))
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
 
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-# def bfs(graph, x, y):
-#     queue = deque()
-#     queue.append((x, y))
-#     graph[x][y] == 1
+def bfs(graph, x, y):
+    queue = deque()
+    queue.append((x, y))
+    if graph[x][y] == 1:
+        return False
 
-#     while queue:
-#         x, y = queue.popleft()
-#         for i in range(4):
-#             nx = x + dx[i]
-#             ny = y + dy[i]
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx<0 or nx>=n or ny<0 or ny>=m:
+                continue
+            if graph[nx][ny] == 0:
+                queue.append((nx, ny))
+                graph[nx][ny] = 1
+    return True
+
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if bfs(graph, i, j) == True:
+            cnt +=1 
+
+print(cnt)
+
+
+
+
