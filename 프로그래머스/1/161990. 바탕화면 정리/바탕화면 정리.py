425e01e9947a7ddedaec4014761369efcdf62fd5
Sungmin Choi <sungmin2517@naver.com>
2024-02-19 13:43:32 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,18 +1,10 @@
 def solution(wallpaper):
-    index_list = []
+    a, b = [], []
+
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[i])):
             if wallpaper[i][j] == '#':
-                index_list.append([i, j])
-                
-    index_list.sort()
-    left = index_list[0][0]
-    index_list.sort(key=lambda x : x[1])
-    top = index_list[0][1]
-    
-    index_list.sort(reverse=True)
-    right=index_list[0][0]
-    index_list.sort(reverse=True, key=lambda x:x[1])
-    bottom = index_list[0][1]
-    
-    return([left, top, right+1, bottom+1])
+                a.append(i)
+                b.append(j)
+
+    return [min(a), min(b), max(a)+1, max(b)+1]
