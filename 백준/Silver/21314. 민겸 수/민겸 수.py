61511d9bc51ad94e30d892b75476e736a5e770fa
Sungmin Choi <sungmin2517@naver.com>
2024-04-22 14:14:44 +0900

    [Silver I] Title: 민겸 수, Time: 120 ms, Memory: 110536 KB -BaekjoonHub

---
+++ 백준/Silver/21314. 민겸 수/민겸 수.py
@@ -0,0 +1,24 @@
+data = input()
+max = "" 
+min = "" 
+m = 0
+
+for i in range(len(data)):
+    if data[i] == 'M':
+        m += 1 
+    elif data[i] == 'K':
+        if m: 
+            min += str(10**m + 5) 
+            max += str(5 * (10**m)) 
+        else: 
+            min += "5" 
+            max += "5"
+        m = 0
+if m: 
+    min += str(10 ** (m-1))
+    while m:
+        max += "1"
+        m -= 1
+print(max)
+print(min)
+
