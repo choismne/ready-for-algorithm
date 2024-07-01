36637b735ff87b36751c3898b2cf4a91ce53775a
Sungmin Choi <sungmin2517@naver.com>
2024-07-01 12:32:27 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.21 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int[] array) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int value : array){
+            list.add(value);
+        }
+        Collections.sort(list);
+        return list.get(array.length/2);
+    }
+}
