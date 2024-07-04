368bd99d276ecfb539e53eae7f2d49cc16b4cf93
Sungmin Choi <sungmin2517@naver.com>
2024-07-04 16:01:15 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,12 +1,8 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int[] arr = new int[3];
         int answer = 1;
         for(int i=0 ; i<3 ; i++){
-            arr[i] = box[i]/n;
-        }
-        for(int value : arr){
-            answer *= value;
+            answer *= box[i]/n;
         }
         return answer;
     }
