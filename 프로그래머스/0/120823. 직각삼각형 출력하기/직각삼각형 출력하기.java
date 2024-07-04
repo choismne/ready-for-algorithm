e567fde6377f9033c4ac1b6896efecb0a1317c0a
Sungmin Choi <sungmin2517@naver.com>
2024-07-04 11:55:20 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 165.86 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -5,10 +5,7 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=1 ; i<n+1 ; i++){
-            for(int j=0 ; j<i ; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            System.out.println("*".repeat(i));   
         }
     }
 }
