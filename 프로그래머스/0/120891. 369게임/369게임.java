2754b9033bd4af932c72076e085e54b418db106a
Sungmin Choi <sungmin2517@naver.com>
2024-07-04 16:05:50 +0900

    [level 0] Title: 369게임, Time: 0.18 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -3,9 +3,7 @@ class Solution {
         int answer=0;
         String[] arr = String.valueOf(order).split("");
         for(String value : arr){
-            if(value.equals("3")) answer ++;
-            else if(value.equals("6")) answer ++;
-            else if(value.equals("9")) answer++;
+            if(value.equals("3") || value.equals("6") || value.equals("9")) answer++;
         }
         return answer;
     }
